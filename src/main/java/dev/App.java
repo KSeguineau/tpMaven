90a42998fbb5717b5f8e8be17c58fce87a36cfad
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-04-12 15:40:23 +0200

    remplacement des variables dans pom.xml

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,7 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String a = FigletFont.convertOneLine("Hello World");
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String a = FigletFont.convertOneLine(titre);
         System.out.println( a );
+        
+        System.out.println("environnement : "+ResourceBundle.getBundle("application").getString("environnement"));
     }
 }
